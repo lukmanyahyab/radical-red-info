23f8b29bf4fecff04278dc5937464eb83bf06f66
Lukman Yahya <alphabeticc@gmail.com>
2023-11-01 22:01:14 +0700

    improve Homepage header and add link to Radical Red dev post

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,22 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="m-auto text-center text-3xl">
+    <div className="max-w-[75%] m-auto text-center text-3xl [&>*]:mb-4">
       <p>
-        For people that plays <span className="text-red-500">Radical Red 3.1</span> with randomized
-        abilities.
+        For people that plays{" "}
+        <Link
+          href="https://www.pokecommunity.com/showthread.php?t=437688"
+          target="_blank"
+          className="text-red-500 hover:underline"
+        >
+          Radical Red 3.1
+        </Link>{" "}
+        with randomized abilities.
       </p>{" "}
-      <p>Help you to keep track what's your mons regular and hidden abilities.</p>{" "}
+      <p>
+        Help you keep track what's your Mons regular and hidden abilities and some other useful
+        pieces of information.
+      </p>{" "}
       <p>
         Go here&nbsp;
         <Link href="/abilities" className="text-blue-500 hover:underline">
